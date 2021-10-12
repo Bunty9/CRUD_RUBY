e6c6d453ca443dbdc5567f7b193485f7bc472daf
Bunty9 <cbipin2000@gmail.com>
2021-10-12 19:35:39 +0530

    done almost

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
   end
   def about
+    @about_me = "My name is Bipin S C"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   get 'home/about'
   # get 'home/index'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
 end
